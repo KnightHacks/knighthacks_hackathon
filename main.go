927c0450e932e73c6e299d4659d415de05d46a30
Warren Snipes <contact@warrensnipes.dev>
2022-06-26 23:43:40 -0400

    implemented ConnectWithRetries

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"context"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/KnightHacks/knighthacks_hackathon/graph"
 	"github.com/KnightHacks/knighthacks_hackathon/graph/generated"
 	"github.com/KnightHacks/knighthacks_hackathon/repository"
 	"github.com/KnightHacks/knighthacks_shared/auth"
+	"github.com/KnightHacks/knighthacks_shared/database"
 	"github.com/KnightHacks/knighthacks_shared/pagination"
 	"github.com/KnightHacks/knighthacks_shared/utils"
 	"github.com/gin-gonic/gin"
@@ -24,7 +24,7 @@ func main() {
 		port = defaultPort
 	}
 
-	pool, err := pgxpool.Connect(context.Background(), utils.GetEnvOrDie("DATABASE_URI"))
+	pool, err := database.ConnectWithRetries(utils.GetEnvOrDie("DATABASE_URI"))
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 	}
